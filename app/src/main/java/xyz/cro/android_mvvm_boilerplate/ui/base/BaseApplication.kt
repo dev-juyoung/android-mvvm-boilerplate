7e950d3749cb3a694acdcbe30d3775406449bc76
Juyoung Lee <dev.juyoung@huray.net>
2018-07-31 18:16:08 +0900

    Timber 초기화 로직 추가

--- app/src/main/java/xyz/cro/android_mvvm_boilerplate/ui/base/BaseApplication.kt
+++ app/src/main/java/xyz/cro/android_mvvm_boilerplate/ui/base/BaseApplication.kt
@@ -2,8 +2,10 @@ package xyz.cro.android_mvvm_boilerplate.ui.base
 
 import android.app.Application
 import org.koin.android.ext.android.startKoin
+import timber.log.Timber
 import xyz.cro.android_mvvm_boilerplate.di.appModule
 import xyz.cro.android_mvvm_boilerplate.di.networkModule
+import xyz.cro.android_mvvm_boilerplate.extensions.isDebuggable
 
 class BaseApplication : Application() {
     init {
@@ -13,6 +15,14 @@ class BaseApplication : Application() {
     override fun onCreate() {
         super.onCreate()
         startKoin(this, listOf(appModule, networkModule))
+
+        if (!isDebuggable()) return
+        initializeDevTools()
+    }
+
+    private fun initializeDevTools() {
+        // Timber
+        Timber.plant(Timber.DebugTree())
     }
 
     companion object {
