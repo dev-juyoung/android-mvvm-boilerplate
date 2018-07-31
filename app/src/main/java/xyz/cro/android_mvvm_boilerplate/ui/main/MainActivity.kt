dbfa168aa13c3b6c482debf8255c2098d3ffe269
Juyoung Lee <dev.juyoung@huray.net>
2018-07-31 17:10:01 +0900

    MainActivity의 Package 구조 변경

--- app/src/main/java/xyz/cro/android_mvvm_boilerplate/ui/main/MainActivity.kt
+++ app/src/main/java/xyz/cro/android_mvvm_boilerplate/ui/main/MainActivity.kt
@@ -1,7 +1,8 @@
-package xyz.cro.android_mvvm_boilerplate
+package xyz.cro.android_mvvm_boilerplate.ui.main
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import xyz.cro.android_mvvm_boilerplate.R
 
 class MainActivity : AppCompatActivity() {
 
