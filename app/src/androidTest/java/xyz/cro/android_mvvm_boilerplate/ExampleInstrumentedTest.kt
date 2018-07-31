ee21cb20f21ddccbc906b99d55e02ebc3a15f33c
Juyoung Lee <dev.juyoung@huray.net>
2018-07-31 11:03:06 +0900

    Initialize Commit

---
+++ app/src/androidTest/java/xyz/cro/android_mvvm_boilerplate/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package xyz.cro.android_mvvm_boilerplate
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getTargetContext()
+        assertEquals("xyz.cro.android_mvvm_boilerplate", appContext.packageName)
+    }
+}
