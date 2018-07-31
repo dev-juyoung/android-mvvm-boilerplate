a1fd682c61e3660bdcab0165c827452c675fca38
Juyoung Lee <dev.juyoung@huray.net>
2018-07-31 16:58:29 +0900

    SharedPreferences DI 추가

--- app/src/main/java/xyz/cro/android_mvvm_boilerplate/di/AppModule.kt
+++ app/src/main/java/xyz/cro/android_mvvm_boilerplate/di/AppModule.kt
@@ -1,7 +1,10 @@
 package xyz.cro.android_mvvm_boilerplate.di
 
+import android.content.Context
+import android.content.SharedPreferences
 import org.koin.dsl.module.applicationContext
 
 val appModule = applicationContext {
-
+    // Dependency For Shared Preferences
+    bean { get<Context>().getSharedPreferences("app-pref", Context.MODE_PRIVATE) as SharedPreferences }
 }
