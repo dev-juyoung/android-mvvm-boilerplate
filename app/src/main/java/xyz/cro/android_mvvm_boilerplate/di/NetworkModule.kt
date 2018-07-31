9cc6be060a83a4ad7852ab480d25d6818bb1057b
Juyoung Lee <dev.juyoung@huray.net>
2018-07-31 17:33:40 +0900

    OkHtttp / Retrofit 관련 Network DI Module 기본 골격 작성

--- app/src/main/java/xyz/cro/android_mvvm_boilerplate/di/NetworkModule.kt
+++ app/src/main/java/xyz/cro/android_mvvm_boilerplate/di/NetworkModule.kt
@@ -1,7 +1,29 @@
 package xyz.cro.android_mvvm_boilerplate.di
 
+import okhttp3.Interceptor
+import okhttp3.OkHttpClient
 import org.koin.dsl.module.applicationContext
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import xyz.cro.android_mvvm_boilerplate.BuildConfig
+import xyz.cro.android_mvvm_boilerplate.network.AuthenticationInterceptor
 
 val networkModule = applicationContext {
+    bean(KeySet.BEAN_HEADERS) {
+        AuthenticationInterceptor(get()) as Interceptor
+    }
 
+    bean(KeySet.BEAN_HTTP_CLIENT) {
+        OkHttpClient.Builder()
+                .addInterceptor(get(KeySet.BEAN_HEADERS))
+                .build()
+    }
+
+    bean(KeySet.BEAN_API_SERVER) {
+        Retrofit.Builder()
+                .baseUrl(BuildConfig.SERVER_URL)
+                .client(get(KeySet.BEAN_HTTP_CLIENT))
+                .addConverterFactory(GsonConverterFactory.create())
+                .build()
+    }
 }
