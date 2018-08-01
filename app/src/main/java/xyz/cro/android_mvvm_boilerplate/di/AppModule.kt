1ae611292a4213bac4dde592e45293e51480aa5d
Juyoung Lee <dev.juyoung@huray.net>
2018-08-01 11:43:44 +0900

    PreferencesRepository 의존성 추가

--- app/src/main/java/xyz/cro/android_mvvm_boilerplate/di/AppModule.kt
+++ app/src/main/java/xyz/cro/android_mvvm_boilerplate/di/AppModule.kt
@@ -3,9 +3,12 @@ package xyz.cro.android_mvvm_boilerplate.di
 import android.content.Context
 import android.content.SharedPreferences
 import org.koin.dsl.module.applicationContext
+import xyz.cro.android_mvvm_boilerplate.data.source.sharedpref.PreferencesDataContract
+import xyz.cro.android_mvvm_boilerplate.data.source.sharedpref.PreferencesRepository
 import xyz.cro.android_mvvm_boilerplate.util.Constants
 
 val appModule = applicationContext {
     // Dependency For Shared Preferences
     bean { get<Context>().getSharedPreferences(Constants.PREFERENCES_NAME, Context.MODE_PRIVATE) as SharedPreferences }
+    bean { PreferencesRepository(get()) as PreferencesDataContract }
 }
