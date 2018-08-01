8f21ee0beff898f2dee8832fef82602b72205ffe
Juyoung Lee <dev.juyoung@huray.net>
2018-08-01 13:57:36 +0900

    getLayoutResId 추상  메서드 ArchitectureActivity -> BaseActivity 로 이동

--- app/src/main/java/xyz/cro/android_mvvm_boilerplate/ui/base/ArchitectureActivity.kt
+++ app/src/main/java/xyz/cro/android_mvvm_boilerplate/ui/base/ArchitectureActivity.kt
@@ -3,12 +3,10 @@ package xyz.cro.android_mvvm_boilerplate.ui.base
 import android.databinding.DataBindingUtil
 import android.databinding.ViewDataBinding
 import android.os.Bundle
-import android.support.annotation.LayoutRes
 
 abstract class ArchitectureActivity<B : ViewDataBinding, VM : BaseViewModel> : BaseActivity() {
     protected abstract val viewModel: VM
     protected lateinit var viewDataBinding: B
-    @LayoutRes protected abstract fun getLayoutResId(): Int
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
