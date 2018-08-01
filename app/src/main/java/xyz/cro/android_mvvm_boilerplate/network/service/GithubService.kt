4e81b5397a56e2b7d52ca3bec5316d69bc885dcf
Juyoung Lee <dev.juyoung@huray.net>
2018-08-01 13:49:24 +0900

    Github 유저 Repositories 조회 API 추가

--- app/src/main/java/xyz/cro/android_mvvm_boilerplate/network/service/GithubService.kt
+++ app/src/main/java/xyz/cro/android_mvvm_boilerplate/network/service/GithubService.kt
@@ -1,4 +1,11 @@
 package xyz.cro.android_mvvm_boilerplate.network.service
 
+import io.reactivex.Observable
+import retrofit2.http.GET
+import retrofit2.http.Path
+import xyz.cro.android_mvvm_boilerplate.data.model.GithubRepoResponse
+
 interface GithubService {
+    @GET("users/{user}/repos")
+    fun getUserRepositories(@Path("user") user: String): Observable<List<GithubRepoResponse>>
 }
