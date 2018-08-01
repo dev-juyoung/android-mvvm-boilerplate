71132bce450ac07ee0b6eca863dc555cbdd6a057
Juyoung Lee <dev.juyoung@huray.net>
2018-08-01 11:07:04 +0900

    메모리 Leakk 방지 및 RxJava의 참조를 관리하기 위한 CompositeDisposable 추가 및 onCleared 시 해제 로직 추가

--- app/src/main/java/xyz/cro/android_mvvm_boilerplate/ui/base/BaseViewModel.kt
+++ app/src/main/java/xyz/cro/android_mvvm_boilerplate/ui/base/BaseViewModel.kt
@@ -1,9 +1,13 @@
 package xyz.cro.android_mvvm_boilerplate.ui.base
 
 import android.arch.lifecycle.ViewModel
+import io.reactivex.disposables.CompositeDisposable
 
 class BaseViewModel : ViewModel() {
+    protected val compositeDisposable = CompositeDisposable()
+
     override fun onCleared() {
         super.onCleared()
+        compositeDisposable.clear()
     }
 }
