206f992177ae36ae3b400c68e7012f8a60476292
Juyoung Lee <dev.juyoung@huray.net>
2018-08-02 10:12:45 +0900

    Glide의 정의된 내용을 Application에 Inline 처리 하기 위한 Module 추가

---
+++ app/src/main/java/xyz/cro/android_mvvm_boilerplate/util/ApplicationGlideModule.kt
@@ -0,0 +1,7 @@
+package xyz.cro.android_mvvm_boilerplate.util
+
+import com.bumptech.glide.annotation.GlideModule
+import com.bumptech.glide.module.AppGlideModule
+
+@GlideModule
+class ApplicationGlideModule : AppGlideModule()
